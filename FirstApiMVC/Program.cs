66bcc29e2ad88ecd9ca898d3a12c76db73e5ecb4
Shahrriar Haque <shahriarhaque445@gmail.com>
2024-06-05 14:55:57 +0600

    update frontend ip

--- FirstApiMVC/Program.cs
+++ FirstApiMVC/Program.cs
@@ -63,7 +63,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://127.0.0.1:5500")
+        builder.WithOrigins("http://127.0.0.1:5501")
                .AllowAnyMethod()
                .AllowAnyHeader();
     });
